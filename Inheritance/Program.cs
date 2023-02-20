e80141f15b74a5c52a1e162a3e069fcdd3c6ca3f
Tom Settle <tom.k.settle@gmail.com>
2023-02-20 17:04:38 -0600

    Completed.

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace Inheritance
 {
@@ -20,9 +21,6 @@ namespace Inheritance
             // give this class 4 members that are specific to Reptile
             // Set this class to inherit from your Animal Class
 
-
-
-
             /*Create an object of your Bird class
              *  give values to your members using the object of your Bird class
              *  
@@ -34,6 +32,97 @@ namespace Inheritance
              *  
              * Creatively display the class member values 
              */
+            var thisBird = new Bird("Cardinal", 0.25m, 3, 2, 6, false, true, 10);
+            var thisReptile = new Reptile("Alligator", 150, 20, 4, 18, true, 15, true);
+
+            Console.WriteLine("Animal List");
+            Console.WriteLine(thisBird.SigLong());
+            Console.WriteLine(thisReptile.SigLong());
         }
     }
+
+    public class Animal
+    {
+        public string Name { get; set; }
+        public decimal? Weight { get; set; }         //lbs
+        public int? LifeExpectency { get; set; }
+        public int? Legs { get; set; }
+
+        public Animal() 
+        {
+        }
+
+        public Animal(string nm, decimal? wt, int? life, int? lgs)
+        {
+            Name= nm;
+            Weight= wt;
+            LifeExpectency= life;
+            Legs = lgs;
+        }
+
+        public string SigShort()
+        {
+            return $"{Name} @ {Weight}lbs with {Legs} legs";
+        }
+    }
+
+    public class Bird : Animal
+    {
+        public int? AirSpeed { get; set; }       //mph
+        public bool? DoesMigrate { get; set; }
+        public bool? DoesFly { get; set; }
+        public int? Range { get; set; }          //miles
+
+        public Bird()
+        {
+        }
+
+        public Bird(string nm, decimal? wt, int? life, int? lgs, int? speed, bool? migrates, bool? flies, int? rng)
+        {
+            Name= nm;
+            Weight= wt;
+            LifeExpectency= life;
+            Legs= lgs;
+            AirSpeed = speed;
+            DoesMigrate = migrates;
+            DoesFly = flies;
+            Range= rng;
+        }
+
+        public string SigLong()
+        {
+            return $"{Name} @ {Weight}lbs with {Legs} legs, Migration:{DoesMigrate}, Flight Range:{Range} @ {AirSpeed}mph";
+        }
+    }
+
+    public class Reptile : Animal
+    {
+        public int? LandSpeed { get; set; }         //mph
+        public bool? DoesSwim { get; set; }
+        public int? WaterSpeed { get; set; }        //mph
+        public bool? BearsEggs { get; set; }
+
+
+        public Reptile()
+        {
+        }
+
+        public Reptile(string nm, decimal? wt, int? life, int? lgs, int? land, bool? swims, int? water, bool? eggs)
+        {
+            Name= nm;
+            Weight = wt;
+            LifeExpectency= life;
+            Legs= lgs;
+            LandSpeed= land;
+            DoesSwim = swims;
+            WaterSpeed= water;
+            BearsEggs= eggs;
+        }
+
+        public string SigLong()
+        {
+            return $"{Name} @ {Weight}lbs with {Legs} legs, Speed:{LandSpeed}mph by land / {WaterSpeed}mph by water, Bears Eggs:{BearsEggs}";
+        }
+    }
+
 }
